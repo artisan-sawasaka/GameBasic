8502b948ab8e3ec2b38aab469948992ec43f3d26
Tomoyuki Sawasaka <tomoyuki.sawasaka@gu3.co.jp>
2017-03-07 02:55:03 +0900

    ラスタースクロールシェーダを追加

--- source/shader/RasterScrollShader.cpp
+++ source/shader/RasterScrollShader.cpp
@@ -4,8 +4,76 @@
 
 #define SAFE_RELEASE(a) if (a != nullptr) { a->Release(); a = nullptr; }
 
+static const char* fx = {
+	"// -------------------------------------------------------------\n"
+	"// テクスチャ\n"
+	"// -------------------------------------------------------------\n"
+	"texture SrcMap;\n"
+	"float Offset = 0.0f;\n"
+	"float Period = 0.0f;\n"
+	"float Amplitude = 0.24f;\n"
+	"\n"
+	"sampler SrcSamp = sampler_state\n"
+	"{\n"
+	"    Texture = <SrcMap>;\n"
+	"    MinFilter = LINEAR;\n"
+	"    MagFilter = LINEAR;\n"
+	"    MipFilter = NONE;\n"
+	"\n"
+	"    AddressU = BORDER;\n"
+	"    AddressV = BORDER;\n"
+	"};\n"
+	"\n"
+	"// -------------------------------------------------------------\n"
+	"// ピクセルシェーダプログラム\n"
+	"// -------------------------------------------------------------\n"
+	"float4 PS_pass1(float2 uv : TEXCOORD0) : COLOR\n"
+	"{\n"
+	"    float2 dst;\n"
+	"    dst.x = uv.x + sin((uv.y + Offset) * Period) * Amplitude;\n"
+	"    dst.y = uv.y;\n"
+	"\n"
+	"    float4 Color = tex2D(SrcSamp, dst);\n"
+	"    return Color;\n"
+	"}\n"
+	"\n"
+	"// -------------------------------------------------------------\n"
+	"// テクニック\n"
+	"// -------------------------------------------------------------\n"
+	"technique TShader\n"
+	"{\n"
+	"    pass P0\n"
+	"    {\n"
+	"        // シェーダ\n"
+	"        PixelShader  = compile ps_2_0 PS_pass1();\n"
+	"        \n"
+	"        Sampler[0] = (SrcSamp);\n"
+	"    }\n"
+	"}\n"
+};
+
 RasterScrollShader::RasterScrollShader()
 {
+	auto device = DeviceManager::GetInstance()->GetDevice()->GetDevice();
+	if (device == nullptr) return ;
+
+	//ファイルの読み込み
+	LPD3DXBUFFER message;
+	HRESULT hr = D3DXCreateEffect(device, fx, strlen(fx), nullptr, nullptr, 0, nullptr, &effect_, &message);
+	if (FAILED(hr)) {
+		std::string s = static_cast<const char*>(message->GetBufferPointer());
+		SAFE_RELEASE(message);
+		return;
+	}
+	SAFE_RELEASE(message);
+	techniqe_ = effect_->GetTechniqueByName("TShader");
+	offset_handle_ = effect_->GetParameterByName(nullptr, "Offset");
+	amplitude_handle_ = effect_->GetParameterByName(nullptr, "Amplitude");
+	period_handle_ = effect_->GetParameterByName(nullptr, "Period");
+
+	effect_->GetFloat(offset_handle_, &offset_);
+	effect_->GetFloat(amplitude_handle_, &amplitude_);
+	effect_->GetFloat(period_handle_, &period_);
 }
 
 RasterScrollShader::~RasterScrollShader()
@@ -13,11 +81,20 @@ RasterScrollShader::~RasterScrollShader()
 	Release();
 }
 
-bool RasterScrollShader::LoadFile(const char* path)
+void RasterScrollShader::SetOffset(float value)
 {
-	if (!Shader::LoadFile(path)) {
-		return false;
-	}
+	offset_ = value;
+	effect_->SetFloat(offset_handle_, value);
+}
 
-	return true;
+void RasterScrollShader::SetAmplitude(float value)
+{
+	amplitude_ = value;
+	effect_->SetFloat(amplitude_handle_, value);
+}
+
+void RasterScrollShader::SetPeriod(float value)
+{
+	period_ = value;
+	effect_->SetFloat(period_handle_, value);
 }
--- source/shader/RasterScrollShader.h
+++ source/shader/RasterScrollShader.h
@@ -12,7 +12,40 @@ public :
 	RasterScrollShader();
 	~RasterScrollShader();
 
-	virtual bool LoadFile(const char* path);
+	virtual bool LoadFile(const char* path) { return false; }
+
+	/*!
+	 * @brief 縦の移動値を設定
+	 */
+	void SetOffset(float value);
+	/*!
+	 * @brief 横の移動値を設定
+	 */
+	void SetPeriod(float value);
+	/*!
+	 * @brief 1カーブ辺りの大きさを設定
+	 */
+	void SetAmplitude(float value);
+
+	/*!
+	 * @brief 縦の移動値を取得
+	 */
+	float GetOffset() const { return offset_; }
+	/*!
+	 * @brief 横の移動値を設定
+	 */
+	float GetPeriod() const { return period_; }
+	/*!
+	 * @brief 1カーブ辺りの大きさを設定
+	 */
+	float GetAmplitude() const { return amplitude_; }
 
 private :
+	D3DXHANDLE offset_handle_;
+	D3DXHANDLE amplitude_handle_;
+	D3DXHANDLE period_handle_;
+	
+	float offset_;
+	float amplitude_;
+	float period_;
 };
